434526fc1c454d8457f5b186bcb898478f2f17f1
Ozoano <obiuchenna74@gmail.com>
2024-10-30 12:29:26 +0100

    initial commit

---
+++ src/Components/Input.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+function Input({setTodos}) {
+  const [text, setText] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setTodos((prevTodos) => ([
+     ...prevTodos,
+        { id: crypto.randomUUID(), title: text }
+      ])
+    )
+
+    setText("")
+  }
+
+  return (
+    
+    // form to collect data
+    <form onSubmit={handleSubmit}>
+      <input
+      value={text} 
+      placeholder="Type your Todo"
+      onChange={(e) => setText(e.target.value)} 
+      />
+    </form>
+  )
+}
+
+export default Input;
---
+++ src/Components/Todos.jsx
@@ -0,0 +1,15 @@
+import TodoItem from "./TodoItem"
+
+function Todos({todos}){
+
+  return (
+      <ul>
+        {todos.map((todo) =>
+        <TodoItem key={todo.id}
+        todo={todo}
+        />)}
+      </ul>
+    )
+}
+
+export default Todos
